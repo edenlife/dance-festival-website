c75442e6ca4d70bdc395b2cc38e38b60060712d5
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-02-02 18:36:57 +0100

    update gift image

--- store/api.js
+++ store/api.js
@@ -3,11 +3,3 @@ import { fetchWrapper } from './base'
 export const requestCompanyCarebox = (requestDetails) => {
   return fetchWrapper(`company`).post(requestDetails)
 }
-
-export const requestCustomiseCarebox = (requestDetails) => {
-  return fetchWrapper(`customize`).post(requestDetails)
-}
-
-export const orderCarebox = (orderDetails) => {
-  return fetchWrapper(`ordercarebox`).post(orderDetails)
-}

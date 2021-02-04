befdf3758d4d3d6fec1cae61329399c02b21dd0c
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-02-04 07:50:58 +0100

    fix design

--- pages/gifting/checkout.vue
+++ pages/gifting/checkout.vue
@@ -438,12 +438,12 @@
                   <p>Payment Successful</p>
                 </div>
                 <svg
-                  @click="closePage"
                   width="14"
                   height="14"
                   viewBox="0 0 14 14"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  @click="closePage"
                 >
                   <path
                     d="M13 1L1 13"
@@ -485,12 +485,12 @@
                   <span>Quantity: {{ item.quantity }}</span>
                 </div>
               </div>
-              <p>&#8358; {{ currencyFormat(item.amount * item.quantity) }}</p>
+              <p>NGN {{ currencyFormat(item.amount * item.quantity) }}</p>
             </div>
           </div>
           <div class="checkout__summary-footer">
             <p>Subtotal</p>
-            <p>&#8358; {{ currencyFormat(subTotal) }}</p>
+            <p>NGN {{ currencyFormat(subTotal) }}</p>
           </div>
         </div>
       </section>

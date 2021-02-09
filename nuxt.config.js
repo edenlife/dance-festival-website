0a296c1b9f357e36d17ac947ffe5bd659d78f81a
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-02-09 12:17:56 +0100

    add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,6 +85,32 @@ export default {
       body: true,
       defer: true,
     },
+
+    // Google Analytics
+    {
+      innerHTML:
+        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-140804740-1', 'auto');ga('send', 'pageview');",
+      type: 'text/javascript',
+      defer: true,
+    },
+
+    // Global site tag (gtag.js) - Google Analytics
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=UA-140804740-1',
+      type: 'text/javascript',
+      body: true,
+      defer: true,
+    },
+    {
+      innerHTML: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'UA-140804740-1');`,
+      type: 'text/javascript',
+      body: true,
+      defer: true,
+    },
   ],
 
   // Global CSS: https://go.nuxtjs.dev/config-css

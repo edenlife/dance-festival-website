45b9474657471a5c62e4277f43f3f2de092bf132
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-03-25 05:39:15 +0100

    add search blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -59,7 +59,7 @@
       </header>
     </div>
     <!--  -->
-    <div class="container--nav">
+    <div v-if="!showSearchbar" class="container--nav">
       <nav class="nav">
         <div
           v-for="(tab, index) in tabs"
@@ -105,12 +105,179 @@
             />
           </svg>
 
-          <input id="" type="search" name="" placeholder="Search this blog" />
+          <input
+            id=""
+            v-model="search"
+            type="search"
+            name=""
+            placeholder="Search this blog"
+            @keyup.enter="searchPost"
+          />
         </div>
       </nav>
     </div>
     <!--  -->
-    <div class="container--posts">
+    <div v-else class="container--search">
+      <div class="nav">
+        <div class="nav__search nav__search-full">
+          <svg
+            width="19"
+            height="19"
+            viewBox="0 0 19 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.81019 11.7371C3.51629 13.3986 4.85347 14.7115 6.52757 15.387C8.20167 16.0625 10.0756 16.0453 11.737 15.3393C13.3984 14.6332 14.7113 13.296 15.3869 11.6219C16.0624 9.94778 16.0452 8.07389 15.3391 6.41245C14.633 4.75101 13.2958 3.43812 11.6217 2.76259C9.94765 2.08706 8.07376 2.10423 6.41232 2.81032C4.75088 3.51642 3.43799 4.8536 2.76246 6.5277C2.08693 8.2018 2.1041 10.0757 2.81019 11.7371V11.7371Z"
+              stroke="#798B83"
+              stroke-width="1.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M14.1666 14.167L18.3333 18.3337"
+              stroke="#798B83"
+              stroke-width="1.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+
+          <input
+            id=""
+            v-model="search"
+            type="search"
+            name=""
+            placeholder="Search"
+            @keyup.enter="searchPost"
+          />
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div v-if="showSearchbar" class="container--result">
+      <div class="result">
+        <h3 class="result__title">Showing results for “{{ search }}”</h3>
+        <div v-show="!searchResults.length" class="result__empty">
+          <p>Sorry. There are no results for this keyword.</p>
+          <p>
+            Try searching for something else or
+            <span class="" @click.prevent="viewAllPost()"
+              >go back to our main blog page</span
+            >.
+          </p>
+        </div>
+        <div v-show="searchResults.length" class="result__nav">
+          <p class="result__nav-filter">Filter results by category:</p>
+          <div
+            v-for="(tab, index) in tabs"
+            :key="index"
+            class="result__nav-item"
+            @click.prevent="resultTabIndex = tab.id"
+          >
+            <p :class="`${resultTabIndex === tab.id ? 'active' : ''}`">
+              {{ tab.title }}
+            </p>
+            <svg
+              v-if="resultTabIndex === tab.id"
+              width="6"
+              height="6"
+              viewBox="0 0 6 6"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="3" cy="3" r="3" fill="#61DB98" />
+            </svg>
+          </div>
+        </div>
+
+        <div v-if="searchResults.length" class="result__content">
+          <div v-for="(item, i) in searchResults" :key="i">
+            <nuxt-link
+              :to="{
+                name: 'blog-slug',
+                params: { slug: item.slug + '?' + item.id },
+              }"
+            >
+              <figure class="result__item">
+                <img
+                  class="result__item-img"
+                  :src="item._embedded['wp:featuredmedia'][0].source_url"
+                  alt=""
+                />
+                <figcaption class="result__item-details">
+                  <h5>{{ item.title.rendered }}</h5>
+                  <p v-html="truncate(item.excerpt.rendered, 180)"></p>
+                  <div class="result__item-date">
+                    <span
+                      :style="getColor(item._embedded['wp:term'][0][0].slug)"
+                      @click.prevent="
+                        getCategory(item._embedded['wp:term'][0][0].id)
+                      "
+                    >
+                      {{ item._embedded['wp:term'][0][0].name }}
+                    </span>
+                    <span class="dot">&#8226;</span>
+                    <span>{{ dateFormatter(item.date) }}</span>
+                  </div>
+                  <div class="result__item-author">
+                    <img
+                      :src="require(`~/assets/images/customer-kofo.jpg`)"
+                      alt=""
+                    />
+                    <p>{{ item._embedded.author[0].name }}</p>
+                  </div>
+                </figcaption>
+              </figure>
+            </nuxt-link>
+          </div>
+        </div>
+        <div v-else class="result__content">
+          <h3 class="result__content-title">Suggested posts</h3>
+          <div v-for="(item, i) in allPosts" :key="i">
+            <nuxt-link
+              :to="{
+                name: 'blog-slug',
+                params: { slug: item.slug + '?' + item.id },
+              }"
+            >
+              <figure class="result__item">
+                <img
+                  class="result__item-img"
+                  :src="item._embedded['wp:featuredmedia'][0].source_url"
+                  alt=""
+                />
+                <figcaption class="result__item-details">
+                  <h5>{{ item.title.rendered }}</h5>
+                  <p v-html="truncate(item.excerpt.rendered, 180)"></p>
+                  <div class="result__item-date">
+                    <span
+                      :style="getColor(item._embedded['wp:term'][0][0].slug)"
+                      @click.prevent="
+                        getCategory(item._embedded['wp:term'][0][0].id)
+                      "
+                    >
+                      {{ item._embedded['wp:term'][0][0].name }}
+                    </span>
+                    <span class="dot">&#8226;</span>
+                    <span>{{ dateFormatter(item.date) }}</span>
+                  </div>
+                  <div class="result__item-author">
+                    <img
+                      :src="require(`~/assets/images/customer-kofo.jpg`)"
+                      alt=""
+                    />
+                    <p>{{ item._embedded.author[0].name }}</p>
+                  </div>
+                </figcaption>
+              </figure>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div v-else class="container--posts">
       <div v-if="activeTabIndex === 0" class="posts">
         <div class="posts__upper">
           <div class="posts__featured">
@@ -479,6 +646,9 @@ export default {
   data() {
     return {
       activeTabIndex: null,
+      resultTabIndex: 0,
+      search: '',
+      showSearchbar: false,
       tabs: [
         { id: 0, title: 'All' },
         { id: 2, title: 'Home' },
@@ -500,6 +670,8 @@ export default {
       lifePosts: [],
       foodPosts: [],
       edenPosts: [],
+      searchResults: [],
+      suggestedPost: [],
     }
   },
   validations: {
@@ -542,6 +714,18 @@ export default {
       } else {
         return value
       }
+    },
+    viewAllPost() {
+      this.showSearchbar = !this.showSearchbar
+    },
+    searchPost() {
+      this.searchResults = this.allPosts.filter((el) =>
+        el.title.rendered.toLowerCase().includes(this.search.toLowerCase())
+      )
+      this.showSearchbar = true
+      // console.log(this.allPosts)
+      // console.log(this.allPosts.splice(1))
+      // this.suggestedPost = this.allPosts.splice(1)
     },
     async fetchAllPosts() {
       const posts = await fetch(

7b262a6c1f10456a3f508770d292dfbace4b3a40
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-03-31 13:30:57 +0100

    integrate disqus for comments

--- plugins/disqus.js
+++ plugins/disqus.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueDisqus from 'vue-disqus'
 
+// TODO change to ouredenlife on prod
+
 Vue.use(VueDisqus, {
-  shortname: 'ouredenlife',
+  shortname: 'ouredenlife-staging',
 })

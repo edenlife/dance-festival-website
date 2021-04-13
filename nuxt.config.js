f6894a18ff3aa32c66206d57d435dbb427a802b3
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-04-13 14:49:42 +0100

    add google tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ export default {
       defer: true,
     },
 
-    // Global site tag (gtag.js) - Google Analytics
+    // Google Analytics
     {
       src: 'https://www.googletagmanager.com/gtag/js?id=UA-140804740-1',
       type: 'text/javascript',
@@ -98,6 +98,25 @@ export default {
       body: true,
       defer: true,
     },
+
+    // Google tag manager
+    {
+      src: 'https://www.googletagmanager.com/ns.html?id=GTM-566V4B2',
+      type: 'text/javascript',
+      body: true,
+      defer: true,
+    },
+    {
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-566V4B2');`,
+      type: 'text/javascript',
+      body: true,
+      defer: true,
+    },
+
     // setup intercom
     {
       innerHTML: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/s0gimx8q';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`,

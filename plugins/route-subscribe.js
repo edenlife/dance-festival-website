717d0a8f42d1617e40b28e74ffc2d18de87a0a19
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-04-30 12:50:34 +0100

    add re-route

--- plugins/route-subscribe.js
+++ plugins/route-subscribe.js
@@ -1,6 +1,7 @@
 export default ({ app }, inject) => {
   window.onNuxtReady(() => {
     const origin = window.location.origin
+    console.log(window.location.origin)
     if (!origin.includes('edenlife')) {
       app.router.push('/signup')
     }

1386939995ae68db427667174882ac0c80fac27c
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-06-03 13:00:45 +0100

    fix route bug

--- plugins/route-subscribe.js
+++
@@ -1,9 +0,0 @@
-export default ({ app }, inject) => {
-  window.onNuxtReady(() => {
-    const origin = document.referrer
-    console.log({ origin })
-    if (origin.includes('facebook')) {
-      app.router.push('/lunch')
-    }
-  })
-}

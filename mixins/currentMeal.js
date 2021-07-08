1eab60a34ff2506fa27054d8776e230ffc2f25d7
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-07-08 09:22:42 +0100

    current meal clean up

---
+++ mixins/currentMeal.js
@@ -0,0 +1,128 @@
+import dayjs from 'dayjs'
+
+export default {
+  data() {
+    return {
+      tabs: [],
+      activeTabIndex: null,
+      firstDateFormat: null,
+      lastDateFormat: null,
+      allMeal: [],
+      mealsInCategory: [],
+    }
+  },
+  mounted() {
+    this.fetchMeal()
+  },
+  methods: {
+    optimizeImage(imgUrl, index) {
+      const imageUrlTrim = imgUrl.substring(0, imgUrl.indexOf('/upload'))
+      const imageFormat = imgUrl.substring(imgUrl.indexOf('/upload') + 7)
+      if (index % 4 === 0) {
+        return (
+          imageUrlTrim +
+          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:E4E1DD10' +
+          imageFormat
+        )
+      }
+      if (index % 3 === 0) {
+        return (
+          imageUrlTrim +
+          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:DDE0E410' +
+          imageFormat
+        )
+      }
+      if (index % 2 === 0) {
+        return (
+          imageUrlTrim +
+          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:D7E3E510' +
+          imageFormat
+        )
+      } else {
+        return (
+          imageUrlTrim +
+          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:D7E3E510' +
+          imageFormat
+        )
+      }
+    },
+    fetchMeal() {
+      this.lastDateFormat = dayjs(new Date())
+        .endOf('week')
+        .format('DD MMM YYYY')
+      this.firstDateFormat = dayjs(new Date())
+        .startOf('week')
+        .format('DD MMM YYYY')
+      const dateData = dayjs(new Date()).format('DD-MM-YYYY')
+      // TODO change to staging
+      fetch(
+        `https://api.edenlife.ng/api/v2/meal/items/all?current_date=${dateData}`
+      )
+        .then((res) => res.json())
+        .then((meals) => {
+          this.allMeal = meals.data
+          const combo = []
+          this.allMeal.map((item) => {
+            return item.preset_combos_full.map((el) => {
+              if (el.visible === true) {
+                combo.push({
+                  name: item.name,
+                  class_category: item.class_category,
+                  ...el,
+                })
+              }
+              return combo
+            })
+          })
+          this.allMeal.map((item) => {
+            if (
+              item.class_category.includes('juice') &&
+              item.combo_image_url !== null &&
+              item.id !== 3864
+            ) {
+              combo.push({
+                name: item.name,
+                class_category: item.class_category,
+                image: item.combo_image_url,
+              })
+            }
+            return combo
+          })
+          this.allMeal.map((item) => {
+            if (item.class_category.includes('smoothies')) {
+              combo.push({
+                name: item.name,
+                class_category: item.class_category,
+                image: item.image_url,
+              })
+            }
+            return combo
+          })
+          this.allMeal = combo
+          this.getMealCategories(this.allMeal)
+        })
+    },
+    getMealCategories(items) {
+      const mapped = items.reduce((acc, { class_category }) => {
+        if (class_category) {
+          const classes = class_category.split(',')
+          acc.push(...classes)
+        }
+        return acc
+      }, [])
+      this.tabs = [...new Set(mapped)]
+      this.activeTabIndex = this.tabs[0]
+      this.changeCategory(this.activeTabIndex)
+    },
+    changeCategory(val) {
+      this.activeTabIndex = val
+      this.mealsInCategory = this.getMealsInEachCategory(this.allMeal, val)
+    },
+    getMealsInEachCategory(items, category) {
+      return items.filter(
+        ({ class_category }) =>
+          class_category && class_category.includes(category)
+      )
+    },
+  },
+}
--- pages/lagos_is_hard.vue
+++ pages/lagos_is_hard.vue
@@ -1020,7 +1020,6 @@
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, email } from 'vuelidate/lib/validators'
-import dayjs from 'dayjs'
 import { mixpanelTrackEvent } from '~/plugins/mixpanel'
 import {
   placeholderColorMix,
@@ -1028,11 +1027,12 @@ import {
   formatNumber,
 } from '~/static/functions'
 import customPricing from '~/mixins/customPricing'
+import currentMeal from '~/mixins/currentMeal'
 import { signupApi } from '~/request/all.api'
 
 export default {
   Loader: () => import('@/components/Loader.vue'),
-  mixins: [validationMixin, customPricing],
+  mixins: [validationMixin, customPricing, currentMeal],
   layout: 'lead',
   validations: {
     form: {
@@ -1050,12 +1050,6 @@ export default {
         phone_number: '',
         discount: '',
       },
-      tabs: [],
-      activeTabIndex: null,
-      firstDateFormat: null,
-      lastDateFormat: null,
-      allMeal: [],
-      mealsInCategory: [],
       loading: false,
       questions: ['one'],
       testimonials: [
@@ -1084,7 +1078,6 @@ export default {
   },
   mounted() {
     mixpanelTrackEvent('Lead page v3')
-    this.fetchMeal()
     window.setInterval(() => {
       this.changeService()
     }, 2300)
@@ -1109,115 +1102,6 @@ export default {
       const first = this.servicesOption.shift()
       this.serviceOption = this.servicesOption[0]
       this.servicesOption = this.servicesOption.concat(first)
-    },
-    optimizeImage(imgUrl, index) {
-      const imageUrlTrim = imgUrl.substring(0, imgUrl.indexOf('/upload'))
-      const imageFormat = imgUrl.substring(imgUrl.indexOf('/upload') + 7)
-      if (index % 4 === 0) {
-        return (
-          imageUrlTrim +
-          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:E4E1DD10' +
-          imageFormat
-        )
-      }
-      if (index % 3 === 0) {
-        return (
-          imageUrlTrim +
-          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:DDE0E410' +
-          imageFormat
-        )
-      }
-      if (index % 2 === 0) {
-        return (
-          imageUrlTrim +
-          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:D7E3E510' +
-          imageFormat
-        )
-      } else {
-        return (
-          imageUrlTrim +
-          '/upload/f_auto,q_auto,e_shadow:100,x_1,y_80,co_rgb:D7E3E510' +
-          imageFormat
-        )
-      }
-    },
-    fetchMeal() {
-      this.lastDateFormat = dayjs(new Date())
-        .endOf('week')
-        .format('DD MMM YYYY')
-      this.firstDateFormat = dayjs(new Date())
-        .startOf('week')
-        .format('DD MMM YYYY')
-      const dateData = dayjs(new Date()).format('DD-MM-YYYY')
-      // TODO change to staging
-      fetch(
-        `https://api.edenlife.ng/api/v2/meal/items/all?current_date=${dateData}`
-      )
-        .then((res) => res.json())
-        .then((meals) => {
-          this.allMeal = meals.data
-          const combo = []
-          this.allMeal.map((item) => {
-            return item.preset_combos_full.map((el) => {
-              if (el.visible === true) {
-                combo.push({
-                  name: item.name,
-                  class_category: item.class_category,
-                  ...el,
-                })
-              }
-              return combo
-            })
-          })
-          this.allMeal.map((item) => {
-            if (
-              item.class_category.includes('juice') &&
-              item.combo_image_url !== null &&
-              item.id !== 3864
-            ) {
-              combo.push({
-                name: item.name,
-                class_category: item.class_category,
-                image: item.combo_image_url,
-              })
-            }
-            return combo
-          })
-          this.allMeal.map((item) => {
-            if (item.class_category.includes('smoothies')) {
-              combo.push({
-                name: item.name,
-                class_category: item.class_category,
-                image: item.image_url,
-              })
-            }
-            return combo
-          })
-          this.allMeal = combo
-          this.getMealCategories(this.allMeal)
-        })
-    },
-    getMealCategories(items) {
-      const mapped = items.reduce((acc, { class_category }) => {
-        if (class_category) {
-          const classes = class_category.split(',')
-          acc.push(...classes)
-        }
-        return acc
-      }, [])
-      this.tabs = [...new Set(mapped)]
-      this.activeTabIndex = this.tabs[0]
-      this.changeCategory(this.activeTabIndex)
-    },
-    changeCategory(val) {
-      this.activeTabIndex = val
-      this.mealsInCategory = this.getMealsInEachCategory(this.allMeal, val)
-    },
-    getMealsInEachCategory(items, category) {
-      return items.filter(
-        ({ class_category }) =>
-          class_category && class_category.includes(category)
-      )
     },
     async getStarted() {
       this.$v.form.$touch()

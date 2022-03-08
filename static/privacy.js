9e8c07e4a4fc005decf9e948572947dbb72c60c3
Cynthia <olucynakin@gmail.com>
2022-03-08 09:02:15 +0100

    privacy policy updates

---
+++ static/privacy.js
@@ -0,0 +1,233 @@
+export default [
+  {
+    id: '',
+    title: 'Privacy Policy',
+    content: ` <div class="content__text"> <strong>Your Privacy Matters</strong>
+        <br />
+        <br />
+        Eden Life’s number one priority is your trust. Your privacy is essential to earning and keeping that trust and we are committed to keeping that trust. This tenet drives all of the decisions we make, as well as how we gather, use and store any information we acquire from you.
+        <br/> <br/>
+        We created this Privacy Policy to be as clear and direct as possible about how we gather and use your nonpublic personal information and other types of personal information (“Personal Information”) and to assist you in exercising your privacy rights.
+<br />
+<br />
+<br />
+<strong>Introduction</strong>
+
+<br/><br/> Eden Life Inc. and/or its affiliates (“Eden Life”, “we”, “us” and/or “our”) currently offer a service that enables you to automatically schedule, manage and have certain household chores and other errands performed on your behalf via your own “Gardener”. Upon payment of subscription fees, you will be assigned one or more individuals (each, a “Gardener”) who will perform those tasks (such as picking up laundry, delivering meals, or household cleaning) requested by you via the Service (each, a “Task”). To schedule a Task, you must input the required information as requested through the Service. You agree that any information you provide when scheduling a Task is true, accurate, current, and complete, and, where applicable, you will update all such information as necessary to maintain its truth, accuracy, and completeness.
+
+<br/> <br/>
+
+For the purpose of this Privacy Policy, a “User” is an individual who creates an account on our Site or App to use our services scheduling App. <br/> <br/>
+
+Our Privacy Policy,     <a
+href="https://ouredenlife.com/terms_and_conditions/"
+>Terms of Use</a>, and, where applicable,    <a
+href="https://ouredenlife.com/terms_and_conditions/#cancellation-policy"
+>Cancellation policy</a>, collectively govern your use or evaluation of our Site, App, and Services.
+
+<br/> <br/>
+<br />
+<br />
+<strong>Information Collection</strong><br/> <br/>
+  <strong> A. The data we collect</strong>
+<br/><br/>
+Eden collects personal data:
+<ul>
+<li> provided by users, such as during account creation </li>
+<li> created during use of our services, such as location, app usage, and device data </li>
+</ul>
+
+Wherever Eden Life collects Personal Information, we endeavor to provide a link to this Privacy Policy and other relevant terms, such as our   <a
+href="https://ouredenlife.com/terms_and_conditions/"
+>Terms of Use</a>.
+<br />
+<br />
+<br />
+
+<strong>Information We Collect from Users</strong>
+<br/><br/>
+We only collect personal information from you when you order for services on our App when you complete any customer survey, or through cookies.
+
+We collect Personal Information from Users, including but not limited to your:
+
+<ul>
+<li>  First name, last name, e-mail address, telephone number, state, Internet Protocol address, birth date.
+</li>
+</ul>
+<br />
+<br />
+<strong>Information Regarding Children </strong>
+<br />
+<br />
+Due to the nature of our business, our Services are not made available to minors. Except for beneficiary information as described above, Eden Life does not knowingly solicit Personal Information directly from or about persons under the age of 16.
+<br />
+<br />
+If you are under the age of 16, please do not submit any Personal Information to Eden Life.
+<br />
+<br />
+If a parent or guardian becomes aware that his or her child under the age of 16 has directly provided us with Personal Information without his or her consent, he or she should contact us at  <a
+href=mailto:support@edenlife.ng"
+>support@edenlife.ng</a> and we will delete such information from our files unless regulatory obligations prevent us from doing so.
+<br />
+<br />
+<br />
+<strong>Other Ways We Collect Information</strong>
+<br />
+<br />
+Other means by which we collect Personal Information include the following:
+
+<ul>
+<li> Automatic Data Collection. We may collect certain information when you use our Services. This information may include your Internet protocol (“IP”) address, cookie identifiers, mobile carrier, mobile advertising and other unique identifiers, details about your browser, operating system or device, location information, Internet service provider, pages that you visit before, during and after using the Services, information about the links you click, and other information about how you use the Services. Information we collect may be associated with accounts and other devices.
+</li><br />
+<li>
+ Anonymized or Aggregated Information. Eden Life’s Site and App record certain anonymized or aggregated information about your use or evaluation of our Services. Anonymized or aggregated information is used for a variety of functions, including the measurement of Users’ and Clients' interest in and use of various portions or features of the Site and App. Anonymized or aggregated information is not Personal Information, and we may use such information in a number of ways, including internal analysis and research. </li><br />
+<li> Cookies and Pixels. Similar to other consumer internet services, Eden Life uses cookies, a small piece of text files that enables our Web servers to “identify” Users and Clients, each time they initiate a session on our Site. A cookie is set in order to identify you and tailor the Site to you. We collect cookies to remember your IP address so that you can save time when coming back to our website. To get more information about cookies, please visit www.allaboutcookies.org. </li><br />
+<li>Cookies do not store any of the Personal Information that you provided to us; they are simply identifiers. You may delete cookie files from your hard drive at any time through your browser settings. However, cookies may be necessary to provide access to much of the content and many of the features of the Site.</li><br />
+<li>Pixel Tags. Along with cookies, we may use “pixel tags,” also known as “web beacons,” which are small graphic files that allow us to monitor the use of our Sites. A pixel tag can collect information such as the IP address of the computer that downloaded the page on which the tag appears; the URL of the page on which the pixel tag appears; the time the page containing the pixel tag was viewed; the type of browser that fetched the pixel tag; and the identification number of any cookie on the computer previously placed by that server.</li><br />
+<li>Site and App Activity. Eden Life may also use third-party tracking technology, such as MixPanel, to record similar information regarding you and your activity on our Site and App.</li><br />
+<li>“Do Not Track” Technology. We do not collect Personal Information about your online activities over time and across different websites or online services. Therefore, our Site does not respond to Do Not Track (“DNT”) signals. We do not knowingly authorize third-parties to collect Personal Information about your online activities over time and across different websites or online services.</li><br />
+<li>Surveys. We may contact you to participate in surveys. If you decide to participate, you may be asked to provide certain information which may include Personal Information.</li>
+</ul>
+
+<br />
+<br />
+<br />
+
+<strong>How we use Information</strong>
+<br />
+<br />
+<strong>No Renting, Selling, or Trading Out List</strong>
+
+<br />
+<br />
+We will never rent, sell or trade your Personal Information to anyone. Ever.
+<br />
+<br />
+<strong>User Personal Information</strong>
+<br />
+<br />
+We use your Personal Information for a variety of business purposes, such as to help you evaluate our Services, offer you new products or services, enhance our Services, and for research and internal analysis.
+
+<br />
+<br />
+<br />
+
+<strong>Client Personal Information</strong>
+<br />
+<br />
+Eden Life stores, processes, and maintains Personal Information related to you for a variety of business reasons such as to provide client support, analyze and improve our Services, offer new products or services. As a User, you may choose not to provide such information to us, but if you choose not to provide such information, you will not be able to become a User.
+<br />
+<br />
+<br />
+<strong>Information Sharing and Onward Transfer</strong><br />
+<br />
+We will not share or disclose your Personal Information (whether you are a current or former User) to any nonaffiliated third-parties except:
+<br />
+<br />
+To Protect Ourselves or Others. We may share your Personal Information as required by law, such as when we reasonably believe it is necessary or appropriate to investigate, prevent, or take action regarding illegal activities, suspected fraud, front running or scalping, situations involving potential threats to the personal safety of any person if we believe doing so is required or appropriate to: comply with law enforcement or national security requests and legal processes, such as a court order or subpoena; to protect your, our or others’ rights, property, or safety; enforce our policies or contracts, or collect amounts owed to us.
+<br />
+<br />
+Service Providers. Eden Life may share your Personal Information among affiliated Eden Life entities in connection with the provision of Services to Users.
+<br />
+<br />
+<br/>
+<strong>Your Choices and Opting-Out</strong>
+<br />
+<br />
+General. You have certain choices about the use and disclosures of your Personal Information, as set out in this Privacy Policy.
+<br />
+<br />
+You may decline to provide Personal Information to Eden Life. Declining to provide Personal Information may disqualify you from using Eden Life Services, Site, and App features that require certain Personal Information.
+<br />
+<br />
+<br/>
+<strong>Opting-Out - Obtaining and Withdrawing Consent</strong>
+ 
+<br />
+<br />
+Where you have consented to Eden Life’s use of your Personal Information, you may withdraw that consent at any time and opt-out by contacting us by email, phone, or physical mail via the contacts indicated under the “Contact Us” section below. Users cannot opt-out of providing Eden Life Personal Information and continue to use the Services. Users must close their account(s) in order to opt-out of further providing Eden Life with Personal Information. Additionally, before we use Personal Information for any new purpose we will provide information regarding the new purpose.
+<br />
+<br />
+<br />
+<strong>Email and Telephone Communications.</strong>
+<br />
+<br />
+ Eden Life may use your Personal Information to communicate with you regarding our Services or to tell you about blog posts or Services that we believe will be of interest to you. If you decide at any time that you no longer wish to receive marketing communications from us, please follow the “unsubscribe” instructions provided in the communications or contact us at   <a
+ href=mailto:support@edenlife.ng"
+>support@edenlife.ng</a>. Please note that you cannot opt-out of administrative communications such as regulatory, billing, or service notifications, or updates to our Terms or this Privacy Policy
+<br />
+<br />
+We process requests to be placed on do-not-mail, do-not-phone, and do-not-contact lists as required by applicable law.
+<br />
+<br />
+<br />
+
+<strong>Mobile Devices</strong>
+<br />
+<br />
+We may send you push notifications through our mobile application. You may at any time opt-out from receiving these types of communications by changing the settings on your mobile device. We may also collect location-based information if you use our mobile applications. You may opt-out of this collection by changing the settings on your mobile device.
+
+<br/>
+<br/>
+<strong>Accessing Your Personal Information</strong>
+<br/>
+<br/>
+Users or Clients may contact us at  <a
+href=mailto:support@edenlife.ng"
+>support@edenlife.ng</a> to request information about how to access your Personal Information.
+<br/>
+ 
+<ul>
+<li>Users can access or amend their Personal Information at any time by signing in to their Eden Life account via our Site or App.</li>
+<li>Your requests will be processed in line with existing laws, including without undue delay and in accordance with any required time frames.</li>
+</ul>
+<br />
+<br />
+<strong>Retention and Deletion</strong>
+<br />
+<br />
+Eden Life retains the Personal Information we receive as described in this Privacy Policy for as long as you use our Site, App, or Services or as necessary to fulfill the purpose(s) for which it was collected, to provide our Services, resolve disputes, establish legal defenses, conduct audits, pursue legitimate business purposes, enforce our agreements, or as otherwise required to comply with contracts and comply with all applicable laws.
+<br />
+<br />
+Users may request information regarding the Personal Information we have collected about you over the last 12 months and/or deletion of Personal Information by contacting us at  <a
+href=mailto:support@edenlife.ng"
+>support@edenlife.ng</a>and providing us enough information to identify your account and prove that you are the owner of the identified account or an authorized representative.
+<br />
+<br />
+<strong>Security</strong>
+<br />
+<br />
+Eden Life takes reasonable steps, endeavoring to use appropriate technical or organizational measures, to protect your Personal Information from loss, misuse, unauthorized access, alteration, disclosure, or destruction. However, no Internet, email, or electronic operating system that enables the transmission of data is ever fully secure or error-free; therefore, we cannot ensure or warrant the security of any information you transmit to us.
+
+<br />
+<br />
+<br />
+<strong>Other Important Information</strong>
+<br />
+<br />
+<strong>Changes to this Privacy Policy</strong>
+<br />
+<br />
+Eden Life encourages you to review this Privacy Policy periodically to be informed regarding how we are using and protecting your information and to be aware of any policy changes. Your continued relationship with Eden Life after the posting or notice of any amended Privacy Policy shall constitute your agreement to be bound by any such changes. Any changes to this Privacy Policy will take effect immediately after being posted or otherwise provided by us. Each version of this Privacy Policy will be identified on this page by its effective date.
+<br />
+<br />
+This document constitutes Eden Life’s complete Privacy Policy for Eden Life and its affiliates and the Services, Site, and App.
+
+<br />
+<br />
+<br />
+<strong>Questions and Contacting Us<strong>
+<br />
+<br />
+If after reviewing this Privacy Policy, you would like to submit a request, opt-out or you have any questions or privacy concerns, please contact us by email at  <a
+href=mailto:support@edenlife.ng"
+>support@edenlife.ng</a> or via physical mail at:
+<br />
+Eden Life Inc
+<br />
+Block C7, Unit 3,
+<br />
+Jacob Mews Estate, Yaba, Lagos.
+ </div> `,
+  },
+]

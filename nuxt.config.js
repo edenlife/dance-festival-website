18cc5cb350723c87603f91b582b61e5c0a07c891
favour-chibueze <chibueze.favour.o@gmail.com>
2022-05-19 14:36:09 +0100

    updated google analytics tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,16 +61,16 @@ export default {
       {
         hid: 'gtm-script1',
         src: 'https://www.googletagmanager.com/ns.html?id=GTM-566V4B2',
-        type: 'text/javascript',
-        defer: true,
-      },
-      {
-        hid: 'gtm-script2',
         innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-566V4B2');`,
+        type: 'text/javascript',
+        defer: true,
+      },
+      {
+        hid: 'gtm-script2',
         body: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-566V4B2"
         height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         type: 'text/javascript',

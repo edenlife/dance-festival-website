6ad0c1af9f3ea627170600208369cccf3143a53c
favour-chibueze <chibueze.favour.o@gmail.com>
2022-09-27 23:35:32 +0100

    Revert "changed env"
    This reverts commit 2f550de5c1ac7415e566d2d1bb13dbb469521c18.

--- static/pricing.js
+++ static/pricing.js
@@ -1,7 +1,6 @@
 import * as pricingLogic from '@edenlife/eden-pricing-module'
 
-// pricingLogic.setEnv("production")
-pricingLogic.setEnv("develop")
+pricingLogic.setEnv(process.env.NODE_ENV === "production" ? "production" : "develop")
 console.log(process.env.NODE_ENV)
 export const pricing = (config) =>
   pricingLogic.pricing({ location: 'NG', config })

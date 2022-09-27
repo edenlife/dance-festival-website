2f550de5c1ac7415e566d2d1bb13dbb469521c18
favour-chibueze <chibueze.favour.o@gmail.com>
2022-09-27 15:14:04 +0100

    changed env

--- static/pricing.js
+++ static/pricing.js
@@ -1,6 +1,7 @@
 import * as pricingLogic from '@edenlife/eden-pricing-module'
 
-pricingLogic.setEnv(process.env.NODE_ENV === "production" ? "production" : "develop")
+// pricingLogic.setEnv("production")
+pricingLogic.setEnv("develop")
 console.log(process.env.NODE_ENV)
 export const pricing = (config) =>
   pricingLogic.pricing({ location: 'NG', config })

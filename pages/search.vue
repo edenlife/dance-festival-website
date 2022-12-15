146809f75ba42fe39c7b0380233fa13a0a835702
Gideon Peters <gideonpeters85@gmail.com>
2022-12-15 14:32:03 +0100

    fix: remove flex

--- pages/search.vue
+++ pages/search.vue
@@ -58,7 +58,7 @@
             <p>Loading menu...</p>
           </div>
           <div v-show="!loading && visibleMealItems.length > 0">
-            <el-row type="flex" :gutter="70">
+            <el-row :gutter="70">
               <el-col
                 v-for="(mealItem, i) in visibleMealItems.filter(
                   (item) => item.category_id === currentTab.id
